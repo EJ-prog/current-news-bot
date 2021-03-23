3649b63053e042beddb3056567ecf1b8a0f131a5
Tirabyte <jeremy.tirador@gmail.com>
2021-03-23 16:04:19 -0700

    Did a thing with the config file

--- src/main/java/com/newsBot/currentNewsBot/newsBotApp.java
+++ src/main/java/com/newsBot/currentNewsBot/newsBotApp.java
@@ -1,6 +1,10 @@
 package com.newsBot.currentNewsBot;
 
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
 import javax.security.auth.login.LoginException;
 
 import net.dv8tion.jda.api.JDA;
@@ -8,11 +12,24 @@ import net.dv8tion.jda.api.JDABuilder;
 
 public class newsBotApp {
    public static void main(String[] theArgs) {
-      // Some code about the key
-      try {
-         JDA api = JDABuilder.createDefault(BOT_TOKEN).build();
-      } 
-      catch (LoginException e) {
+      try (InputStream input = newsBotApp.class.getClassLoader()
+                               .getResourceAsStream("config.properties")) {
+         
+         Properties prop = new Properties();
+
+         if (input != null) {
+            prop.load(input);
+
+            String BOT_TOKEN = prop.getProperty("db.bot_token");
+            try {
+               JDA api = JDABuilder.createDefault(BOT_TOKEN).build();
+            } catch (LoginException e) {
+               e.printStackTrace();
+            }
+         } else {
+            System.out.println("Huh... Something broke... \nGotta fix that!");
+         }
+      } catch (IOException e) {
          e.printStackTrace();
       }
    }

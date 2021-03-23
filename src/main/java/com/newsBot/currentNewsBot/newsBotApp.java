76f09906a85bf5f361d510d4da3ea3df91904ae5
Tirabyte <jeremy.tirador@gmail.com>
2021-03-23 15:51:06 -0700

    Some progress on event listeners and framework on the bot

--- src/main/java/com/newsBot/currentNewsBot/newsBotApp.java
+++ src/main/java/com/newsBot/currentNewsBot/newsBotApp.java
@@ -1,7 +1,19 @@
-package main.java.com.newsBot.currentNewsBot;
+package com.newsBot.currentNewsBot;
+
+
+import javax.security.auth.login.LoginException;
+
+import net.dv8tion.jda.api.JDA;
+import net.dv8tion.jda.api.JDABuilder;
 
 public class newsBotApp {
    public static void main(String[] theArgs) {
-      
+      // Some code about the key
+      try {
+         JDA api = JDABuilder.createDefault(BOT_TOKEN).build();
+      } 
+      catch (LoginException e) {
+         e.printStackTrace();
+      }
    }
 }

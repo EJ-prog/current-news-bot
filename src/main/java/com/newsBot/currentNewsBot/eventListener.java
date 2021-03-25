5d46b1cb333ea19863c74f4fa62ddec4215ea02f
Tirabyte <jeremy.tirador@gmail.com>
2021-03-24 22:03:16 -0700

    Added some simple commands! (News, Opinion, and Help)

--- src/main/java/com/newsBot/currentNewsBot/eventListener.java
+++ src/main/java/com/newsBot/currentNewsBot/eventListener.java
@@ -10,12 +10,28 @@ public class eventListener extends ListenerAdapter {
    public void onMessageReceived(MessageReceivedEvent theEvent) {
       if (!theEvent.getAuthor().isBot()) {
          System.out.println("I'm not bot!");
+
          Message message = theEvent.getMessage();
          String content = message.getContentDisplay();
+         MessageChannel channel = theEvent.getChannel();
+
          if (content.equals("!ping")) {
+            // Ping pong Command
             System.out.println("I heard ping! Sending Pog!");
-            MessageChannel channel = theEvent.getChannel();
             channel.sendMessage("Pong!").queue();
+         } else if (content.equals("!news")) {
+            // Gives you news on demand
+            channel.sendMessage("Tis' the news!\n (Insert Links Here)");
+         } else if (content.equals("!opinion")) {
+            // Opinions section of news
+            channel.sendMessage("It's an opinion!\n (Insert Links Here)");
+         } else if (content.equals("!help") || content.equals("!halp")) {
+            // Gives help
+            channel.sendMessage("Never Fear! Uni News Bot is Here! \\o/\n"+
+               "!ping - Gives pong! \n" +
+               "!news - Shows Current articles from Uni!\n" +
+               "!opinion - Shows Opinion articles from Uni!\n" +
+               "!help - Gives you help! (You are here)").queue();
          }
       }
    }
